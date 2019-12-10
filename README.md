137357e8ddba8d9fec63c820e95450e91a9b0270
Alex Skreen <skreenalex@gmail.com>
2019-12-09 19:21:19 -0800

    Add README.md

---
+++ README.md
@@ -0,0 +1,38 @@
+# _JavaScript & jQuery Independent Project_
+#### _This is my second project at Epicodus_
+#### By _Alex Skreen_
+#### https://github.com/alexskreen/JS-Project
+## Descripton
+
+This is a program I created to determine which coding language someone should choose as their first language. I used HTML, CSS, Bootstrap, JavaScript, and jQuery
+
+## Setup/Installation Requirements
+
+* _open desktop_
+* _open terminal_
+* _clone repository from GitHub_
+* _open in atom by using atom . in command line_
+* _click and drag index.html into your browser, or double click index.html in folder_
+
+## Known Bugs
+
+* _No known bugs_
+
+## Support and Contact Details
+
+* Email support@alexskreen.com for any necessary support
+
+## Technologies Used
+* _HTML_,
+* _CSS_,
+* _Bootstrap_,
+* _JavaScript_,
+* _jQuery_,
+* _Git Hub_,
+* _Atom_,
+* _Stack overflow_,
+
+### Legal
+
+* Copyright (c) 2019 Alex Skreen
+* This software is licensed under the MIT license
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,52 @@
 $(document).ready(function()  {
   var age = parseInt(prompt("How old are?!"));
 
-  if (age === 18) {
-    alert("Getting and early start on your journey to becoming a developer! Congrats!");
-    $('#ofage').show();
-  }
-  else if (age > 18) {
-    $('#ofage').show();
-  }
-  else {
-    alert("I applaud your aspirations but you're not quite old enough for our program!");
-    $('#underage').show();
-    $('#samplepicture').show();
-  }
-  if (interest === "2" && type === "2" && company === "2") {
-  $(".typeOne, .typeThree").hide();
-  $(".typeFour, .typeFive").hide();
-  $(".typeTwo").show();
+if (age === 18) {
+  alert("Getting and early start on your journey to becoming a developer! Congrats!");
+  $('#ofage').show();
 }
+else if (age > 18) {
+  $('#ofage').show();
+}
+else {
+  alert("I applaud your aspirations but you're not quite old enough for our program!");
+  $('#underage').show();
+  $('#samplepicture').show();
+}
+  $("form#questions").submit(function(event) {
+    var aspect = $("#aspect").val();
+    var naturally = $("#naturally").val();
+    var rather = $("#rather").val();
+    var location = $("#location").val();
+    var time = $("#time").val();
+
+
+    if (aspect === "1" && naturally === "1" && rather === "3" && location === "1" && time === "1") {
+      $("#typeTwo, #typeThree").hide();
+      $("#typeFour, #typeFive").hide();
+      $("#typeOne").show();
+    }
+      else if (aspect === "2" && naturally === "2" && rather === "1" && location === "2" && time === "1") {
+        $("#typeOne, #typeThree").hide();
+        $("#typeFour, #typeFive").hide();
+        $("#typeTwo").show();
+      }
+      else if (aspect === "3" && naturally === "1" && rather === "1" && location === "1" && time === "1") {
+        $("#typeOne, #typeTwo").hide();
+        $("#typeFour, #typeFive").hide();
+        $("#typeThree").show();
+      }
+      else if (aspect === "2" && naturally === "2" && rather === "2" && location === "2" && time === "2") {
+        $("#typeOne, #typeTwo").hide();
+        $("#typeThree, #typeFive").hide();
+        $("#typeFour").show();
+      }
+      else if (aspect === "1" && naturally === "1" && rather === "2" && location === "1" && time === "2") {
+        $("#typeOne, #typeTwo").hide();
+        $("#typeFour, #typeThree").hide();
+        $("#typeFive").show();
+      }
+      event.preventDefault();
+
+});
 });
